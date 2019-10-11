aa351838069c5db53345da0382b83acd5a73bfd6
QDSG <piftao1127@163.com>
2019-10-11 14:38:02 +0800

    修改购物车

--- RxSwfitDemo/RxSwfitDemo/Classes/Controllers/TTCartViewController.swift
+++ RxSwfitDemo/RxSwfitDemo/Classes/Controllers/TTCartViewController.swift
@@ -1,5 +1,5 @@
 //
-//  TTCardViewController.swift
+//  TTCartViewController.swift
 //  RxSwfitDemo
 //
 //  Created by QDSG on 2019/10/10.
@@ -7,31 +7,37 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
-class TTCardViewController: UIViewController {
+class TTCartViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var totalItemsLabel: UILabel!
     @IBOutlet weak var totalCostLabel: UILabel!
     @IBOutlet weak var checkoutButton: UIButton!
 
+    private let disposeBag = DisposeBag()
 }
 
-extension TTCardViewController {
+extension TTCartViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "购物车"
+        tableView.tableFooterView = UIView()
         configureFromCart()
+        setupCellConfiguration()
     }
 }
 
-extension TTCardViewController {
+extension TTCartViewController {
     @IBAction func clearCard() {
         TTShoppingCart.shared.commodities.accept([])
         navigationController?.popViewController(animated: true)
     }
 }
 
-extension TTCardViewController {
+extension TTCartViewController {
     func configureFromCart() {
         guard checkoutButton != nil else { return }
         
@@ -40,5 +46,18 @@ extension TTCardViewController {
         totalCostLabel.text = CurrencyFormatter.dollarsFormatter.string(from: cart.totalCost)
         
         checkoutButton.isEnabled = cart.totalCost > 0
+    }
+    
+    func setupCellConfiguration() {
+        let cart = TTShoppingCart.shared
+        
+        cart.commodities
+            .bind(to: tableView
+                .rx
+                .items(cellIdentifier: TTListViewCell.reuseIdentifier, cellType: TTListViewCell.self)) {
+                    (row, commodity, cell) in
+                    cell.configure(with: commodity)
+        }
+        .disposed(by: disposeBag)
     }
 }
